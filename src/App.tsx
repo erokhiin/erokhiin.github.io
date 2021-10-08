e665b3c081a0b68483cb094f5d42d8266749cc4c
erokhiin <33400447+erokhiin@users.noreply.github.com>
2021-10-08 16:04:20 +0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Links } from "./Links";
+
+const App = () => (
+  <div>
+    <h1>My links</h1>
+    <Links />
+  </div>
+);
+
+export default App;
---
+++ src/Links/Links.tsx
@@ -0,0 +1,11 @@
+import styles from "./Links.module.scss";
+
+export const Links = () => (
+  <div className={styles.container}>
+    <a href="https://www.instagram.com/erokhiin.s">Insragram: erokhiin.s</a>
+    <a href="https://www.facebook.com/erokhiin">Facebook: erokhiin</a>
+    <a href="https://www.linkedin.com/in/erokhiin">Linkedin: erokhiin</a>
+    <a href="hrrps://www.github.com/erokhiin">Github: erokhiin</a>
+    <a href="https://www.t.me/erokhiin">Telegram: erokhiin</a>
+  </div>
+);
